2d07ec37f07a51f699b731c7cbce7d693358d044
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2026-09-05 10:01:26 +0200

    Update

--- src/soc.sv
+++ src/soc.sv
@@ -1,13 +1,17 @@
 module soc (
     input clk,
-    output reg [7:0] led
+    output reg[7:0] led = 0
 );
 
-  reg[25:0] ctr = 0;
+  reg[24:0] ctr = 0;
 
   always @(posedge clk) begin
-    ctr <= ctr + 1;
-    led[7:0] <= ctr[25:18];
+    if (ctr < 24999999)
+      ctr <= ctr + 1;
+    else begin
+      ctr <= 0;
+      led <= ~led;
+    end
   end
 
 endmodule
