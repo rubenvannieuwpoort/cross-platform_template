7b1780fa3c04949fa6b76943a87ddc7f64c2a1e1
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2026-09-05 11:01:09 +0200

    Add clock generator to ulx3s

--- platform/ulx3s/clock_gen.sv
+++ platform/ulx3s/clock_gen.sv
@@ -1,12 +1,20 @@
 module clock_gen #(
-    parameter CLKI_DIV,
-    parameter CLKFB_DIV,
-    parameter CLKOP_DIV,
-    parameter CLK1_DIV,
+    // the following parameters must be in the range 1-128
+    parameter CLKI_DIV,  // D
+    parameter CLKFB_DIV, // M
+    parameter CLKOP_DIV, // C
+    parameter CLK1_DIV   // O1
 ) (
     input  wire clk_in,
     output wire clk_out
 );
+
+// f_VCO needs to be in 400-800 MHz
+// generally we want it to be higher to have low jitter
+
+// f_in is 25 MHz for the ULX3S
+// f_VCO = f_in * M * C / D
+// f_1 = f_VCO / O1
 
 wire clk_ref;
 
--- platform/ulx3s/top.sv
+++ platform/ulx3s/top.sv
@@ -6,8 +6,20 @@ module top (
 
   assign wifi_gpio0 = 1'b1;  // keep board from rebooting
 
+  wire clk;
+
+  clock_gen #(
+    .CLKI_DIV(1),
+    .CLKFB_DIV(4),
+    .CLKOP_DIV(4),
+    .CLK1_DIV(8),
+  ) clock_gen (
+    .clk_in(clk_sys),
+    .clk_out(clk)
+  );
+
   soc soc (
-    .clk(clk_sys),
+    .clk(clk),
     .led(led)
   );
 
